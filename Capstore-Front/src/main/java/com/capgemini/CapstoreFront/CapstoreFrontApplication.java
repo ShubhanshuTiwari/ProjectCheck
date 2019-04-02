7bbde15b7c2856587bb6b6d751e783c569c5e7cc
ShubhanshuTiwari <47036297+ShubhanshuTiwari@users.noreply.github.com>
2019-04-02 14:03:14 +0530

    Add files via upload

---
+++ Capstore-Front/src/main/java/com/capgemini/CapstoreFront/CapstoreFrontApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.CapstoreFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoreFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoreFrontApplication.class, args);
+	}
+
+}
